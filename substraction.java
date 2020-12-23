88f4ebb167f463eae5dc0094ff2615e863e82741
gurramramesh <40685905+gurramramesh@users.noreply.github.com>
2020-12-23 22:16:41 +0530

    Create substraction.java

---
+++ substraction.java
@@ -0,0 +1,10 @@
+public class SubTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sub;
+      sub = num1 - num2;
+
+      System.out.println("Sum of these numbers: "sub);
+   }
+}
