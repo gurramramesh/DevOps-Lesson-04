cd5033c7fd6be75848b8e58062ccd4f3191d8d65
gurramramesh <40685905+gurramramesh@users.noreply.github.com>
2020-12-23 22:18:03 +0530

    Create divide.java

---
+++ divide.java
@@ -0,0 +1,10 @@
+public class divTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, div;
+      div = num1 / num2;
+
+      System.out.println("Sum of these numbers: "div);
+   }
+}
